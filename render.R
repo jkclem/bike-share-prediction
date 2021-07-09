b9f3b35805452a0d1c3265e57d98a57dbe6e0ae5
John Clements <jkclements2016@gmail.com>
2021-07-09 18:02:10 -0400

    Made unsuccesful changes to the automation code.

--- render.R
+++ render.R
@@ -6,5 +6,16 @@
 
 library(rmarkdown)
 
-rmarkdown::render("./Analysis.Rmd",
+rmarkdown::render("Analysis.Rmd",
                   params=list(weekday=c(0,1,2,3,4,5,6)))
+
+#output_file <- paste0( weekday, ".html")
+#params<-lapply(weekday, FUN=function(x){list(weekday=x)})
+#reports <- tibble(output_file, params)
+
+apply(reports, MARGIN=1, 
+      FUN=function(x){
+        render(input="Analysis.Rmd",
+               output_file=paste0("Reports\\", x[[1]] ),
+               params=list(weekday=c(0,1,2,3,4,5,6))#x[[2]])
+      }))
